00186fa9668e00c6bca83ab9495cac7ea8fa19da
Revida <revida@users.noreply.github.com>
2017-12-21 11:46:05 +0000

    Add files via upload

---
+++ CacheBusting.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Hosting;
+
+namespace WebApplication1
+{
+	public class CacheBusting
+	{
+		public static string Url(string rootRelativePath)
+		{
+			if (HttpRuntime.Cache[rootRelativePath] == null)
+			{
+				string absolute = HostingEnvironment.MapPath("~" + rootRelativePath);
+
+				DateTime date = File.GetLastWriteTime(absolute);
+				
+				var result = rootRelativePath + "?" + date.Ticks;
+
+				HttpRuntime.Cache.Insert(rootRelativePath, result, new CacheDependency(absolute));
+			}
+
+			return HttpRuntime.Cache[rootRelativePath] as string;			
+		}
+	}
+}
